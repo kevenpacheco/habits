28c08c6cc44756cfa4ff7de7f69af5f32401e9db
Keven Pacheco <kevenpacheco2000@gmail.com>
2023-01-26 12:08:10 -0300

    Second web class

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,16 @@
-import './styles/global.css';
+import "./styles/global.css";
+import { Header } from "./components/Header";
+import { SummaryTable } from "./components/SummaryTable";
 
-import { Habit } from "./components/Habit";
+// import { Habit } from "./components/Habit";
 
-function App() {
+export function App() {
   return (
-    <>
-      <Habit completed={3} />
-      <Habit completed={6} />
-      <Habit completed={42} />
-    </>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+        <SummaryTable />
+      </div>
+    </div>
   );
 }
-
-export default App;
