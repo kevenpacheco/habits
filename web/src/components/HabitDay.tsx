f5e2e50ac16373ee821b77806f684b057d5a1b3b
Keven Pacheco <kevenpacheco2000@gmail.com>
2023-05-04 23:02:01 -0300

    Fifth web class

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,38 +1,51 @@
 import * as Popover from "@radix-ui/react-popover";
-import * as Checkbox from "@radix-ui/react-checkbox";
 import clsx from "clsx";
 import { ProgressBar } from "./ProgressBar";
-import { Check } from "phosphor-react";
 import dayjs from "dayjs";
+import { HabitList } from "./HabitsList";
+import { useState } from "react";
 
 interface HabitDayProps {
   date: Date;
-  completed?: number;
+  defaultCompleted?: number;
   amount?: number;
 }
 
-export function HabitDay({ date, amount = 0, completed = 0 }: HabitDayProps) {
+export function HabitDay({
+  date,
+  amount = 0,
+  defaultCompleted = 0,
+}: HabitDayProps) {
+  const [completed, setCompleted] = useState(defaultCompleted);
+
   const completedPercentege =
     amount > 0 ? Math.round((completed / amount) * 100) : 0;
 
   const dayAndMonth = dayjs(date).format("DD/MM");
   const dayOfWeek = dayjs(date).format("dddd");
 
+  function handleCompletedChanged(completed: number) {
+    setCompleted(completed);
+  }
+
   return (
     <Popover.Root>
       <Popover.Trigger
-        className={clsx("w-10 h-10  border-2  rounded-lg", {
-          "bg-zinc-900 border-zinc-800": completedPercentege === 0,
-          "bg-violet-900 border-violet-700":
-            completedPercentege > 0 && completedPercentege < 20,
-          "bg-violet-800 border-violet-600":
-            completedPercentege >= 20 && completedPercentege < 40,
-          "bg-violet-700 border-violet-500":
-            completedPercentege >= 40 && completedPercentege < 60,
-          "bg-violet-600 border-violet-500":
-            completedPercentege >= 60 && completedPercentege < 80,
-          "bg-violet-500 border-violet-400": completedPercentege >= 80,
-        })}
+        className={clsx(
+          "w-10 h-10  border-2  rounded-lg transition-colors focus:outline-none focus:ring-2 focus:ring-violet-700 focus:ring-offset-2 focus:ring-offset-background",
+          {
+            "bg-zinc-900 border-zinc-800": completedPercentege === 0,
+            "bg-violet-900 border-violet-700":
+              completedPercentege > 0 && completedPercentege < 20,
+            "bg-violet-800 border-violet-600":
+              completedPercentege >= 20 && completedPercentege < 40,
+            "bg-violet-700 border-violet-500":
+              completedPercentege >= 40 && completedPercentege < 60,
+            "bg-violet-600 border-violet-500":
+              completedPercentege >= 60 && completedPercentege < 80,
+            "bg-violet-500 border-violet-400": completedPercentege >= 80,
+          }
+        )}
       />
 
       <Popover.Portal>
@@ -44,19 +57,7 @@ export function HabitDay({ date, amount = 0, completed = 0 }: HabitDayProps) {
 
           <ProgressBar progress={completedPercentege} />
 
-          <div className="mt-6 flex flex-col gap-3">
-            <Checkbox.Root className="flex items-center gap-3 group">
-              <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
-                <Checkbox.Indicator>
-                  <Check size={20} className="text-white" />
-                </Checkbox.Indicator>
-              </div>
-
-              <span className="font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400">
-                Beber 2L de água
-              </span>
-            </Checkbox.Root>
-          </div>
+          <HabitList date={date} onCompletedChanged={handleCompletedChanged} />
 
           <Popover.Arrow height={8} width={16} className="fill-zinc-900" />
         </Popover.Content>
--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -56,7 +56,7 @@ export function NewHabitForm() {
         autoFocus
         value={title}
         onChange={(event) => setTitle(event.target.value)}
-        className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
+        className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400 focus:outline-none focus:ring-2 focus:ring-violet-700 focus:ring-offset-2 focus:ring-offset-zinc-900"
       />
 
       <label htmlFor="" className="font-semibold leading-tight mt-4">
@@ -69,9 +69,9 @@ export function NewHabitForm() {
             key={weekday}
             onCheckedChange={() => handleToggleWeekDay(index)}
             checked={weekDays.includes(index)}
-            className="flex items-center gap-3 group"
+            className="flex items-center gap-3 group focus:outline-none"
           >
-            <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+            <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 transition-colors group-focus:ring-2 group-focus:ring-violet-700 group-focus:ring-offset-2 group-focus:ring-offset-zinc-900">
               <Checkbox.Indicator>
                 <Check size={20} className="text-white" />
               </Checkbox.Indicator>
@@ -84,7 +84,7 @@ export function NewHabitForm() {
 
       <button
         type="submit"
-        className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500"
+        className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500 transition-colors focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-zinc-900"
       >
         <Check size={20} weight="bold" />
         Confirmar
